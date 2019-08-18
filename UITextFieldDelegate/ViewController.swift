fba2957729ca0c7c6f929c3baefea1ac4494d5ba
mohamedosama2074 <iosengks74@gmail.com>
2019-08-19 01:13:40 +0200

    UITextFieldDelegate

--- UITextFieldDelegate/ViewController.swift
+++ UITextFieldDelegate/ViewController.swift
@@ -8,13 +8,62 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController  , UITextFieldDelegate{
 
+    @IBOutlet weak var btnLogin: UIButton!
+    
+    @IBOutlet weak var labelll: UILabel!
+    
+    @IBOutlet weak var textFieldName: UITextField!
+    
+    
+    @IBOutlet weak var textFieldUserName: UITextField!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        self.textFieldName.delegate = self
+        
+        self.textFieldUserName.delegate = self
+        
+    
+        
     }
 
-
+    @IBAction func btn_Action(_ sender: UIButton) {
+        
+       TakeText()
+        
+       
+    }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        
+        self.view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        textField.resignFirstResponder()
+        
+        return true
+    }
+    
+    
+    
+}
+extension ViewController{
+    
+    func TakeText(){
+        
+        let name  = textFieldName.text
+        let password = textFieldUserName.text
+        
+        labelll.text = "Name : \(name!)\n password : \(password!)"
+        
+    }
+    
 }
 
